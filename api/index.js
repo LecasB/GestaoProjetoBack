76c9f9bfab8e0f38611f0a647b377e45c4ad51fa
Tiago Nogueira <tiagoandresn@gmail.com>
2025-05-08 11:04:17 +0100

    swagger

--- api/index.js
+++ api/index.js
@@ -5,11 +5,14 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
+import { swaggerSpec, swaggerUi } from "../utils/swagger.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config({ path: path.join(__dirname, "../.env") });
 const app = express();
+
+const PORT = process.env.PORT || 3000;
 
 mongoose
   .connect(process.env.MONGO_URI, {
@@ -30,6 +33,13 @@ app.use(cors(corsOptions));
 app.options("*", cors(corsOptions));
 app.use(express.json());
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// Rotas
 app.use("/api/v1", testeRouter);
+
+app.listen(PORT, () => {
+  console.log(`🚀 Servidor a correr em http://localhost:${PORT}`);
+});
 
 export default app;
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -6,6 +6,17 @@ const upload = require("../middlewares/uploadMiddleware");
 // Rota para criar usuário com upload de imagem
 router.post("/", upload.single("image"), userController.createUser);
 
+/**
+ * @swagger
+ * /api/v1/login:
+ *   post:
+ *     summary: Lista todos os utilizadores
+ *     tags:
+ *       - Users
+ *     responses:
+ *       200:
+ *         description: Lista obtida com sucesso
+ */
 router.post("/login", userController.loginUser);
 
 module.exports = router;
