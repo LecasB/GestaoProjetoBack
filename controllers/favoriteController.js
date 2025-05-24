8b86215017cc02076c17e9ffab34d1d645b43ab9
Tiago Nogueira <tiagoandresn@gmail.com>
2025-05-24 15:31:35 +0100

    wd

--- controllers/favoriteController.js
+++ controllers/favoriteController.js
@@ -1,4 +1,4 @@
-import Favorito from "../models/User.js";
+import Favorito from "../models/Favorito.js";
 import Item from "../models/Item.js";
 import User from "../models/User.js";
 
@@ -28,15 +28,14 @@ const addFavorite = async (req, res) => {
 
     if (!iduser || !iditem) {
       return res.status(400).json({ error: "Campos não preenchidos" });
-      return res.status(400).json({ error: "Campos não preenchidos" });
     }
 
     if (!(await User.findById(iduser))) {
       return res.status(400).json({ error: "User invalido" });
     }
 
-    if (!(await Item.findById(iduser))) {
-      console.log(await Item.findById(iduser));
+    if (!(await Item.findById(iditem))) {
+      console.log(await Item.findById(iditem));
       return res.status(400).json({ error: "Item invalido" });
     }
 
@@ -49,4 +48,26 @@ const addFavorite = async (req, res) => {
   }
 };
 
-export default { getAllByUserId, addFavorite };
+const deleteFavorite = async (req, res) => {
+  try {
+    const { iduser, iditem } = req.body;
+
+    if (!iduser || !iditem) {
+      return res
+        .status(400)
+        .json({ error: "iduser e iditem são obrigatórios" });
+    }
+
+    const deleted = await Favorito.findOneAndDelete({ iduser, iditem });
+
+    if (!deleted) {
+      return res.status(404).json({ error: "Favorito não encontrado" });
+    }
+
+    return res.status(200).json({ message: "Favorito removido com sucesso" });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+export default { getAllByUserId, addFavorite, deleteFavorite };
--- models/Favorito.js
+++ models/Favorito.js
@@ -2,7 +2,11 @@ import mongoose from "mongoose";
 
 const favoritoSchema = new mongoose.Schema({
   iduser: { type: String, required: true },
-  iditem: { type: String, required: true },
+  iditem: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "items",
+    required: true,
+  },
 });
 
 export default mongoose.model("Favorito", favoritoSchema);
