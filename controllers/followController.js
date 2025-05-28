b6415ef56697427b6e141b6fc6e9f0ae46e3bb9a
Leonardo <cleonardo191@gmail.com>
2025-05-28 21:22:22 +0100

    wd / follow / followers

--- controllers/followController.js
+++ controllers/followController.js
@@ -1,57 +1,61 @@
 import Follow from "../models/Follow.js";
 
 const getFollowerAndFollowingByUserId = async (req, res) => {
-    try {
-        const { userId } = req.params;
+  try {
+    const { id } = req.params;
 
-        if (!userId) {
-            return res.status(400).json({ error: "ID do usuário não fornecido" });
-        }
-
-        const followers = await Follow.find({ following: userId }).populate('follower', 'name email');
-        const following = await Follow.find({ follower: userId }).populate('following', 'name email');
-
-        return res.status(200).json({ followers, following });
-    } catch (error) {
-        return res.status(500).json({ error: error.message });    
+    if (!id) {
+      return res.status(400).json({ error: "ID do usuário não fornecido" });
     }
-}
+
+    const followersDocs = await Follow.find({ idfollowed: id }, "idfollower");
+
+    const followingDocs = await Follow.find({ idfollower: id }, "idfollowed");
+
+    const followers = followersDocs.map((f) => f.idfollower);
+    const following = followingDocs.map((f) => f.idfollowed);
+
+    return res.status(200).json({ followers, following });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
 
 const newFollow = async (req, res) => {
-    try {
-        const { idfollower, idfollowed } = req.body;
+  try {
+    const { idfollower, idfollowed } = req.body;
 
-        if (!idfollower || !idfollowed) {
-            return res.status(400).json({ error: "Campos não preenchidos" });
-        }
-
-        const follow = new Follow({ idfollower, idfollowed });
-        await follow.save();
-
-        return res.status(201).json({ message: "Seguidor adicionado com sucesso" });
-    } catch (error) {
-        return res.status(500).json({ error: error.message });
+    if (!idfollower || !idfollowed) {
+      return res.status(400).json({ error: "Campos não preenchidos" });
     }
-}
+
+    const follow = new Follow({ idfollower, idfollowed });
+    await follow.save();
+
+    return res.status(201).json({ message: "Seguidor adicionado com sucesso" });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
 
 const deleteFollow = async (req, res) => {
-    try {
-        const { idfollower, idfollowed } = req.body;
+  try {
+    const { idfollower, idfollowed } = req.body;
 
-        if (!idfollower || !idfollowed) {
-            return res.status(400).json({ error: "Campos não preenchidos" });
-        }
-
-        await Follow.deleteOne({ idfollower, idfollowed });
-
-        return res.status(200).json({ message: "Seguidor removido com sucesso" });
-    } catch (error) {
-        return res.status(500).json({ error: error.message });
+    if (!idfollower || !idfollowed) {
+      return res.status(400).json({ error: "Campos não preenchidos" });
     }
-}
+
+    await Follow.deleteOne({ idfollower, idfollowed });
+
+    return res.status(200).json({ message: "Seguidor removido com sucesso" });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
 
 export default {
-    getFollowerAndFollowingByUserId,
-    newFollow,
-    deleteFollow
-}
+  getFollowerAndFollowingByUserId,
+  newFollow,
+  deleteFollow,
+};
